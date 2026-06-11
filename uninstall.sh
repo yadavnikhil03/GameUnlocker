4652eb4cb6d060c3acd30617dcbf96105133a66d
Nikhil <nikhil181245@gmail.com>
2026-06-11 10:45:44 +0530

    fix: sort WebUI app list and clean up persistent props on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,9 @@ umount /proc/cpuinfo 2>/dev/null
 rm -rf /data/adb/modules/Game-Unlocker
 rm -f /sdcard/Download/GameUnlocker_Logs.txt
 
+resetprop -p --delete persist.vendor.qti.game.fps
+resetprop -p --delete persist.vendor.thermal.engine.disable
+
 if [ -f "$INFO" ]; then
   while read LINE; do
     if [ "$(echo -n "$LINE" | tail -c 1)" == "~" ]; then
